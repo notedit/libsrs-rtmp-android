9abab66707eb44235d8c04c1bafe1b4838f17125
notedit <notedit@gmail.com>
2016-02-13 13:01:19 +0800

    fix ndk-build

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,14 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
 
+
+TARGET_ARCH_ABI := armeabi-v7a
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE     := libsrs-client
 
 
-LOCAL_C_INCLUDES:= \
-		bionic \
-
-LOCAL_SRC_FILES  := srs_librtmp.cpp 
+LOCAL_SRC_FILES  := \
+		srs_librtmp.cpp 
 
 LOCAL_LDLIBS     := -llog
 
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,1 +1,5 @@
 APP_ABI := armeabi-v7a
+APP_STL := gnustl_static
+
+APP_GNUSTL_FORCE_CPP_FEATURES := rtti
+
